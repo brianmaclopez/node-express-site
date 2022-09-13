f8b5c7cfc82218a45931f434456caf3eec7c3ba0
Lugh_Lámfada <healingphases@gmail.com>
2022-09-13 16:17:42 -0700

    changed view engine to `ejs`; changed html files to `.ejs` fixed routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+console.log('running routes/index.js...')
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('./index.ejs');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('./about.ejs');
+});
+router.get('/contact-me', function(req, res, next) {
+  res.render('./contact-me.ejs');
 });
 
 module.exports = router;
